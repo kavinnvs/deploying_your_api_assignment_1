bead668663091bf6aa27e57e2e7592b19131d255
vs.kavin <kavinvs90@gmail.com>
2025-02-11 12:38:54 +0530

    day

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
+
+//Note that api values can be different depends on the user but all three values should be printed.
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === 'true',
+};
+
+console.log(config);
+
 
 const app = express();
 const port = 3010;
